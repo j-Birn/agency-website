7235ae7dc35b728303637fb88b22ed47404fc7aa
Ilija <nukawanderer@gmail.com>
2022-06-09 20:02:05 +0800

    core

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Starter from "./components/Starter";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Starter />
+    </>
+  );
+}
+
+export default App;
