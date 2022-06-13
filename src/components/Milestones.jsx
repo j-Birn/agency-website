48ad44b59d850aec02108c4b4cced23f4b051d45
Ilija <nukawanderer@gmail.com>
2022-06-13 17:06:51 +0800

    milestones block

---
+++ src/components/Milestones.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Milestones = () => {
+  return (
+    <div className="milestones-container">
+      <h3 className="milestones-title">
+        We successfully completetd 1500+ projects & counting
+      </h3>
+      <div className="milestones">
+        <div className="milestone">
+          <h3 className="milestone-number">1k+</h3>
+          <h3 className="milestone-content">Global customers</h3>
+        </div>
+        <div className="milestone">
+          <h3 className="milestone-number">2k</h3>
+          <h3 className="milestone-content">Compltede projects</h3>
+        </div>
+        <div className="milestone">
+          <h3 className="milestone-number">50+</h3>
+          <h3 className="milestone-content">Expert workers</h3>
+        </div>
+        <div className="milestone">
+          <h3 className="milestone-number">65+</h3>
+          <h3 className="milestone-content">Evevnts sponsored</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Milestones;
